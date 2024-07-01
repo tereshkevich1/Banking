2c92d4a6020e196086f648f6a4cfb87abedce0bd
tereshkevich1 <tereshkevich.cergey@mail.ru>
2024-07-02 01:02:39 +0300

    clickable row

--- app/src/main/java/com/example/banking/accounts_screen/cards/transaction_card/RecentTransactionRow.kt
+++ app/src/main/java/com/example/banking/accounts_screen/cards/transaction_card/RecentTransactionRow.kt
@@ -1,10 +1,10 @@
 package com.example.banking.accounts_screen.cards.transaction_card
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,19 +13,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import com.example.banking.R
 
 @Composable
-fun RecentTransactionRow(onTextClick: () -> Unit) {
+fun RecentTransactionRow(onRowClick: () -> Unit) {
     val innerPadding = dimensionResource(id = R.dimen.inner_padding)
 
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(top = 24.dp, bottom = innerPadding)
+            .clickable { onRowClick() }
     ) {
         Text(
             text = stringResource(id = R.string.recent_transactions),
@@ -34,12 +34,10 @@ fun RecentTransactionRow(onTextClick: () -> Unit) {
         Spacer(modifier = Modifier.weight(1f))
         val textStyle =
             MaterialTheme.typography.bodySmall.merge(TextStyle(color = colorResource(id = R.color.clickable_text_color)))
-        ClickableText(
-            text = AnnotatedString(stringResource(id = R.string.view_all)),
+        Text(
+            text = stringResource(id = R.string.view_all),
             modifier = Modifier.align(Alignment.CenterVertically),
             style = textStyle
-        ) {
-            onTextClick()
-        }
+        )
     }
 }
